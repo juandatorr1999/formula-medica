97c9dc20504644b4fb4defcdc2c23ef75bd1ba9b
Juan David Torres <juandatorr_1999@hotmail.com>
2022-04-06 20:22:09 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'formula-medica',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
